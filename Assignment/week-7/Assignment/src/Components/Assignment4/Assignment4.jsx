67faf9c55fbf0544aab8ad8e81e54cd730c2d3b2
varun-purohit <varunpurohit48@gmail.com>
2024-02-13 15:43:14 +0530

    week-7 Assignment5

--- Assignment/week-7/Assignment/src/Components/Assignment4/Assignment4.jsx
+++ Assignment/week-7/Assignment/src/Components/Assignment4/Assignment4.jsx
@@ -1,5 +1,66 @@
-const Assignment4 = () => {
-  return <div>Assignment4</div>;
-};
+import React, { useState } from "react";
+import "./Assignment4.css";
+
+function Assignment4() {
+  const [wordCount, setWordCount] = useState(null);
+  const [paragraph, setParagraph] = useState("");
+
+  const handleInputChange = (event) => {};
+
+  const words = [
+    "Lorem",
+    "ipsum",
+    "dolor",
+    "sit",
+    "amet",
+    "consectetur",
+    "adipiscing",
+    "elit",
+    "sed",
+    "do",
+    "eiusmod",
+    "tempor",
+    "incididunt",
+    "ut",
+    "labore",
+    "et",
+    "dolore",
+    "magna",
+    "aliqua",
+    // Add more words as needed
+  ];
+  const generateParagraph = () => {
+    let generatedParagraph = "";
+    for (let i = 0; i < wordCount; i++) {
+      const randomIndex = Math.floor(Math.random() * words.length);
+      generatedParagraph += words[randomIndex] + " ";
+    }
+
+    // Remove the extra space at the end and capitalize the first letter
+    generatedParagraph = generatedParagraph.trim();
+    generatedParagraph =
+      generatedParagraph.charAt(0).toUpperCase() + generatedParagraph.slice(1);
+
+    // Add a period at the end of the paragraph
+    generatedParagraph += ".";
+
+    setParagraph(generatedParagraph);
+  };
+
+  return (
+    <div className="container">
+      <h2>Para Generator</h2>
+      <div className="input">
+        <input
+          type="number"
+          placeholder="Enter Number of Words"
+          onChange={(event) => setWordCount(event.target.value)}
+        />
+        <button onClick={generateParagraph}>Generate</button>
+      </div>
+      <p>{paragraph}</p>
+    </div>
+  );
+}
 
 export default Assignment4;
--- Assignment/week-7/Assignment/src/Components/Assignment5/Assignment5.jsx
+++ Assignment/week-7/Assignment/src/Components/Assignment5/Assignment5.jsx
@@ -1,5 +1,55 @@
-const Assignment5 = () => {
-  return <div>Assignment5</div>;
-};
+import { useState, useEffect } from "react";
+import "./Assignment5.css";
+
+function Assignment5() {
+  const [profData, setProfDtata] = useState({});
+  useEffect(() => {
+    const gitData = async () => {
+      const response = await fetch(`https://api.github.com/users/hkirat`);
+      const data = await response.json();
+      console.log("data", data);
+      setProfDtata(data);
+    };
+    gitData();
+  }, []);
+  return (
+    <div className="profile-container">
+      {/* {console.log("avatarurl", console.log(profData))} */}
+      <div className="image">
+        <img
+          src={profData?.avatar_url}
+          alt="Profile Image"
+          className="profile-img"
+        />
+      </div>
+      <div className="text-data">
+        <p>
+          <b>{profData?.name}</b>
+        </p>
+        <p>{profData?.login}</p>
+        <p>{profData?.bio}</p>
+      </div>
+
+      <div className="prof-line"></div>
+
+      <div className="social-data">
+        <div className="followers">
+          <p className="prof-p1">{profData.followers} </p>
+          <p className="prof-p2"> Followers</p>
+        </div>
+        <div className="likes">
+          <p className="prof-p1">{profData.following} </p>
+          <p className="prof-p2"> Following</p>
+        </div>
+        <div className="photos">
+          <p className="prof-p1">{profData.public_repos}</p>
+          <p className="prof-p2"> Repos</p>
+        </div>
+      </div>
+
+      {/* <div className="line"></div> */}
+    </div>
+  );
+}
 
 export default Assignment5;
